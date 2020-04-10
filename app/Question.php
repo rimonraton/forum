6b776fff02a5fc21388b54d6401203e7c19a3ece
Motaharul <rimon.raton@gmail.com>
2020-04-10 21:55:31 +0600

    Generate Question model via artisan model generator

---
+++ app/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+	protected $fillable = ['title', 'body'];
+
+    public function users()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+    	$this->attributes['title'] = $value;
+    	$this->attributes['slug'] = str_slug($value);
+    }
+
+
+    
+}
