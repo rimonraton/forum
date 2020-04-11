462a35a43aa8ba1e5a0f08bf40c2d47e4051b726
Motaharul <rimon.raton@gmail.com>
2020-04-11 22:38:45 +0600

    Implement Question Policy and rewrite Gate Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' =>['index', 'show']]);
+    }
     public function index()
     {
         $questions = Question::with('user')->latest()->paginate(5);
@@ -34,23 +38,26 @@ class QuestionsController extends Controller
 
     public function edit(Question $question)
     {
-        if(\Gate::denies('update-question', $question)){
-            abort(403, "Access denied!");   
-        }
+        // if(\Gate::denies('update-question', $question)){
+        //     abort(403, "Access denied!");   
+        // }
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         $question->update($request->all());
         return redirect('/questions')->with('success', 'Your question has been updated.');
     }
 
     public function destroy(Question $question)
     {
-        if(\Gate::denies('delete-question', $question)){
-            abort(403, "Access denied!");   
-        }
+        // if(\Gate::denies('delete-question', $question)){
+        //     abort(403, "Access denied!");   
+        // }
+        $this->authorize('delete', $question);
         $question->delete();
         return redirect('/questions')->with('success', 'Your question has been deleted.');
     }
---
+++ app/Policies/QuestionPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Question;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class QuestionPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user, Question $question)
+    {
+        return $user->id == $question->user_id;
+    }
+
+    public function delete(User $user, Question $question)
+    {
+        return $user->id == $question->user_id && $question->answers < 1;
+    }
+
+}
