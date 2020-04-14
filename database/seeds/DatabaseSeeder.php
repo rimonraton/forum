7e0951b79f490caa81691520d7a3dc019120067f
Motaharul <rimon.raton@gmail.com>
2020-04-14 20:01:38 +0600

    Generate fake Answers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	factory(App\User::class, 3)->create()->each(function($u){
-    		$u->questions()->saveMany(
-    			factory(App\Question::class, rand(1, 10))->make()
-    		);
-    	});
+    	factory(App\User::class, 3)->create()
+            ->each(function($u){
+    		  $u->questions()->saveMany(
+        		factory(App\Question::class, rand(1, 10))->make())
+                ->each(function($q){
+                    $q->answers()->saveMany(
+                        factory(App\Answer::class, rand(1, 5))->make()
+                    );
+                });
+    	   });
     }
 }
