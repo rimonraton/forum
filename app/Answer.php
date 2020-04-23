526e6f34b32943789dd7d90cea12b1cf65c2e198
Motaharul <rimon.raton@gmail.com>
2020-04-23 23:47:13 +0600

    Update best answer id on questions table when answer deleted in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -27,13 +27,7 @@ class Answer extends Model
         });
 
         static::deleted(function($answer) {
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if($question->best_answer_id == $answer->id) {
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
-
+            $answer->question->decrement('answers_count');
         });
     }
     public function getCreatedDateAttribute()
