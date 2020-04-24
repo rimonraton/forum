ba2c969a6c15a0cb58aa0caef705b7f3b9a06c8f
Motaharul <rimon.raton@gmail.com>
2020-04-24 19:53:23 +0600

    Implement accept the answer as best answer

--- app/Policies/AnswerPolicy.php
+++ app/Policies/AnswerPolicy.php
@@ -15,11 +15,15 @@ class AnswerPolicy
     {
         return $user->id == $answer->user_id;
     }
-
    
     public function delete(User $user, Answer $answer)
     {
         return $user->id == $answer->user_id;
+    }
+
+    public function accept(User $user, Answer $answer)
+    {
+        return $user->id == $answer->question->user_id;
     }
 
 }
--- app/Question.php
+++ app/Question.php
@@ -46,8 +46,11 @@ class Question extends Model
     {
         return $this->hasMany(Answer::class);
     }
+    public function acceptBestAnswer(Answer $answer)
+    {
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
     
 
-
-    
 }
