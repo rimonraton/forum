a14785fb6b2d75f1f2ad7cb404a34e41ebfe8119
Motaharul <rimon.raton@gmail.com>
2020-04-29 22:07:28 +0600

    Tidy up the views

---
+++ resources/views/answers/_answer.blade.php
@@ -0,0 +1,33 @@
+<div class="media post">
+	@include('shared._vote', [
+		'model' => $answer
+	])
+	<div class="media-body">
+		{!! $answer->body_html !!}
+		<div class="row">
+			<div class="col-4">
+				<div class="ml-auto">
+					@can('update', $answer)
+						<a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+					@endcan
+					@can('delete', $answer)
+						<form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+							@method('DELETE')
+							@csrf
+							<button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')"> Delete
+							</button>
+						</form>
+					@endcan
+				</div>
+			</div>
+			<div class="col-4"></div>
+			<div class="col-4">
+				@include('shared.author', [
+					'label' => 'Answered',
+					'model' => $answer
+				])
+			</div>
+		</div>
+		
+	</div>
+</div>		
